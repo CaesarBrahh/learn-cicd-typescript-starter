8091b6893eee613dbfcacc0d8ea347bb6b6ec554
CaesarBrahh <julaenw@gmail.com>
2026-08-18 22:28:04 -0500

    temporarily broke src/auth.ts to ensure new CI workflow catches failure

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -4,7 +4,8 @@ export function getAPIKey(headers: IncomingHttpHeaders): string | null {
   // stores the value of the authorization header from the incoming HTTP request headers object and check if that value is present
   const authHeader = headers["authorization"];
   if (!authHeader) {
-    return null;
+    //return null;
+    return "test failure";
   }
 
   // Splits and returns only the second part "Apikey abc123" --> "abc123"
